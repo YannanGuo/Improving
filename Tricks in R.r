83d6b8be556feba9c675270499116b3ab17cc7ae
Guo Yannan <53413128+YannanGuo@users.noreply.github.com>
2023-06-13 23:46:09 +0800

    Update Tricks in R.r

--- Tricks in R.r
+++ Tricks in R.r
@@ -11,3 +11,13 @@ lapply(pkgs,function(pkg){
     if(!require(pkg,quietly = TRUE))
     BiocManager::install(pkg,update = FALSE)
 })
+
+
+#Missing value cleaning
+##Using function 'complete.cases()', This function returns a logical vector indicating which rows in res1 have complete cases, 
+##meaning they have no missing values. The value TRUE indicates a complete case, while FALSE indicates a row with missing values.
+res2 <- res1[complete.cases(res1),]
+
+#The function of this code:
+#The code filters out any rows in res1 that contain missing values and assigns the filtered result to res2. 
+#This can be useful when working with data that has missing values, as it allows you to focus on complete cases for further analysis or modeling.
